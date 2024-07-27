6fd96dc16b5318ac89c707c0d60cc6660b012831
dingqingchen <ding0489@foxmail.com>
2024-07-28 01:52:07 +0800

    FreeList: convert freelist write logic from serial to parallel

--- src/main/scala/core/backend/rename/FreeList.scala
+++ src/main/scala/core/backend/rename/FreeList.scala
@@ -6,36 +6,42 @@ import chisel3.util._
 import core._
 import core.general._
 
-class FreeListIO(implicit p: Parameter) extends CoreBundle {
-  val req = Flipped(DecoupledIO(Vec(RenameWidth, Bool())))
-  val rsp = Output(Vec(RenameWidth, UInt(PhyRegIdxWidth.W)))
-  val free = Flipped(ValidIO(Vec(CommitWidth, ValidIO(UInt(PhyRegIdxWidth.W)))))
-}
-
 class FreeList(size: Int)(implicit p: Parameter) extends CoreModule {
-  val io = IO(new FreeListIO)
-  val freeList = RegInit(VecInit.tabulate(size)(i => (i + 32).U(PhyRegIdxWidth.W)))
+  val io = IO(new Bundle {
+    val allocate = new Bundle {
+      val req = Flipped(DecoupledIO(Vec(RenameWidth, Bool())))
+      val rsp = Output(Vec(RenameWidth, UInt(PhyRegIdxWidth.W)))
+    }
+    val free = Flipped(ValidIO(Vec(CommitWidth, ValidIO(UInt(PhyRegIdxWidth.W)))))
+  })
+  val freeList = RegInit(VecInit.tabulate(size)(i => (i + NRArchRegs).U(PhyRegIdxWidth.W)))
   val zeroPtr = 0.U.asTypeOf(CirclePtr(size))
   val rptrNext, wptrNext = WireInit(zeroPtr)
-  val rptr = RegEnable(rptrNext, zeroPtr, io.req.fire)
-  val wptr = RegEnable(wptrNext, (NRPhyRegs - 32).U.asTypeOf(zeroPtr), io.free.valid)
-  rptrNext := rptr + PopCount(io.req.bits)
+  val rptr = RegEnable(rptrNext, zeroPtr, io.allocate.req.fire)
+  val wptr = RegEnable(wptrNext, (NRPhyRegs - NRArchRegs).U.asTypeOf(zeroPtr), io.free.valid)
+  rptrNext := rptr + PopCount(io.allocate.req.bits)
   wptrNext := wptr + PopCount(io.free.bits.map(_.valid))
-  val rptrOH = RegEnable(UIntToOH(rptrNext.asUInt, size), 1.U(size.W), io.req.fire)
-  val rptrOHs = VecInit.tabulate(RenameWidth)(i => rptrOH.rotateLeft(PopCount(io.req.bits.take(i))))
-  io.rsp.zipWithIndex.foreach { case (v, i) => v := Mux1H(rptrOHs(i), freeList) }
+  val rptrOH = RegEnable(UIntToOH(rptrNext.asUInt, size), 1.U(size.W), io.allocate.req.fire)
+  val rptrOHs = VecInit.tabulate(RenameWidth)(i => rptrOH.rotateLeft(PopCount(io.allocate.req.bits.take(i))))
+  io.allocate.rsp.zipWithIndex.foreach { case (v, i) => v := Mux1H(rptrOHs(i), freeList) }
   val readEntryNumNext = wptr - rptr
-  io.req.ready := RegNext(readEntryNumNext >= RenameWidth.U, false.B)
-  val wptrOH = RegEnable(UIntToOH(wptrNext.asUInt, size), 1.U(size.W), io.free.valid)
-  val wptrOHs = VecInit.tabulate(CommitWidth)(i => wptrOH.rotateLeft(PopCount(io.free.bits.map(_.valid).take(i))))
-  //TODO: convert the freelist write logic from serial to parallel
+  io.allocate.req.ready := RegNext(readEntryNumNext >= RenameWidth.U, false.B)
+  val freeListNextValidVec = VecInit.fill(size, CommitWidth)(false.B)
+  val freeListNextVec = VecInit.fill(size, CommitWidth)(0.U(PhyRegIdxWidth.W))
+  val freeListNextValid = VecInit.tabulate(size)(i => freeListNextValidVec(i).reduce(_ | _))
+  val freeListNext = VecInit.tabulate(size)(i => Mux(freeListNextValid(i), freeListNextVec(i).reduce(_ | _), freeList(i)))
   when(io.free.valid) {
     io.free.bits.zipWithIndex.foreach { case (v, i) =>
       when(v.valid) {
-        freeList(wptrOHs(i)) := v.bits
+        val ptr = wptr + PopCount(io.free.bits.map(_.valid).take(i))
+        freeListNextVec(ptr.value)(i) := v.bits
+        freeListNextValidVec(ptr.value)(i) := true.B
       }
     }
+    freeList := freeListNext
+    freeListNextValid.zipWithIndex.foreach { case (v, i) => assert(PopCount(v) <= 1.B, s"multiply free phy register $i") }
   }
+
 }
 
 object FreeList extends App {
